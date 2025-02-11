362878b11d6a60125b73657c9e1f7c086d638565
Elliot Chan <elliot.chan@student.tdsb.on.ca>
2025-02-11 15:18:51 +0000

    finish practice poroblem

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,32 @@ public class PracticeProblem {
 
 	}
 
+	public static int sum(int num1, int num2) {
+		return num1 + num2;
+	}
+	public static int difference(int num1, int num2) {
+		return num1 - num2;
+	}
+	public static double product(double num1, double num2) {
+		return num1*num2;
+	}
+	public static String removeFirst(String s) {
+		return s.substring(1);
+	}
+	public static int combinedLength(String s1, String s2) {
+		String combined = s1 + "" + s2;
+		return combined.length();
+	}
+	public static boolean isEven(int num) {
+		return num % 2 == 0;
+	}
+	public static boolean isOdd(int num) {
+		return num % 2 != 0;
+	}
+	public static boolean isPositive(int num) {
+		return num > 0;
+	}
+	public static boolean isNegative(int num) {
+		return num < 0;
+	}
 }
